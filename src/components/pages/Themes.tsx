7e9d0139e2aab838649455230773d803c3a02e4e
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-11-02 02:46:35 +0900

    テーマ一覧取得

--- src/components/pages/Themes.tsx
+++ src/components/pages/Themes.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useContext } from "react"
 
 import { makeStyles } from "@material-ui/core/styles"
 import { Grid, Typography } from "@material-ui/core"
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
 
 import Dialog from "@material-ui/core/Dialog"
 import DialogContent from "@material-ui/core/DialogContent"
@@ -15,7 +17,7 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder"
 import AlertMessage from "components/utils/AlertMessage"
 
 import { getThemes, postTheme } from "lib/api/themes"
-import { Theme, PostThemeRequest, PostThemeResponse } from "interfaces/theme"
+import { GetThemesResponse, PostThemeRequest, PostThemeResponse } from "interfaces/theme"
 import { User } from "interfaces/user"
 
 import { AuthContext } from "App"
@@ -27,7 +29,7 @@ const Themes: React.FC = () => {
   const { currentUser } = useContext(AuthContext)
 
   const [loading, setLoading] = useState<boolean>(true)
-  const [themes, setThemes] = useState<Theme[]>([])
+  const [responseData, setResponseData] = useState<GetThemesResponse[]>([])
   const [themeTitle, setThemeTitle] = useState<string>("")
 //   const [user, setUser] = useState<User>(initialUserState)
 //   const [userDetailOpen, setUserDetailOpen] = useState<boolean>(false)
@@ -64,7 +66,6 @@ const Themes: React.FC = () => {
         console.log("Failed")
       }
 
-      
     } catch (err) {
       console.log(err)
     }
@@ -77,32 +78,16 @@ const Themes: React.FC = () => {
       console.log(res)
 
       if (res?.status === 200) {
-        setThemes(res?.data.themes)
-        console.log(themes);
+        setResponseData(res?.data.themes)
+        console.log(responseData);
       } else {
-        console.log("No themes")
+        console.log("No Data")
       }
     } catch (err) {
       console.log(err)
     }
     setLoading(false)
   }
-
-//   // いいね一覧を取得
-//   const handleGetLikes = async () => {
-//     try {
-//       const res = await getLikes()
-//       console.log(res)
-
-//       if (res?.status === 200) {
-//         setLikedUsers(res?.data.activeLikes)
-//       } else {
-//         console.log("No likes")
-//       }
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
 
   useEffect(() => {
     handleGetThemes()
@@ -112,36 +97,47 @@ const Themes: React.FC = () => {
     <>
       {
         !loading ? (
-          themes?.length > 0 ? (
-            <Grid container justify="center">
-              <input
-                type="text"
-                placeholder="テーマのタイトル"
-                value={themeTitle}
-                onChange={(e) => setThemeTitle(e.target.value)}
-              />
-              <button onClick={handleCreateTheme}>テーマを作成</button>
+          responseData?.length > 0 ? (
+            <Grid container justify="center" spacing={2}>
+              <Grid item xs={12}>
+                <input
+                  type="text"
+                  placeholder="テーマのタイトル"
+                  value={themeTitle}
+                  onChange={(e) => setThemeTitle(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <button onClick={handleCreateTheme}>テーマを作成</button>
+              </Grid>
               {
-                themes?.map((theme: Theme, index: number) => {
+                responseData?.map((theme: GetThemesResponse, index) => {
+                  const updatedAtDate = new Date(theme.updatedAt);
+                  const formattedDate = `${updatedAtDate.getFullYear()}/${(updatedAtDate.getMonth() + 1).toString().padStart(2, '0')}/${updatedAtDate.getDate().toString().padStart(2, '0')} ${updatedAtDate.getHours().toString().padStart(2, '0')}:${updatedAtDate.getMinutes().toString().padStart(2, '0')}`;
+
                   return (
-                    <div>
-                      <Grid item style={{ margin: "0.5rem", cursor: "pointer" }}>
-                        {/* <Avatar
-                          alt="avatar"
-                          src={user?.image.url}
-                          className={classes.avatar}
-                        /> */}
-                        <Typography
-                          variant="body2"
-                          component="p"
-                          gutterBottom
-                          style={{ marginTop: "0.5rem", textAlign: "center" }}
-                        >
-                          <Link to={`/${theme.user?.name}/themes/${theme.themeId}`}>{theme.title} {theme.user?.name}</Link>
-                        </Typography>
-                      </Grid>
-                    </div>  
-                  ) 
+                    <Grid item xs={12} key={index}>
+                      <Card>
+                        <CardHeader
+                          avatar={<Avatar alt="avatar" src={theme.user.image} />}
+                          title={
+                            <Typography variant="body2" component="p" gutterBottom>
+                              <Link to={`/${theme.user.name}`} style={{ color: 'black', textDecoration: 'none' }}>
+                                {theme.user.name} {formattedDate}
+                              </Link>
+                            </Typography>
+                          }
+                        />
+                        <CardContent>
+                          <Typography variant="body1" component="p">
+                            <Link to={`/${theme.user.name}/themes/${theme.themeId}`} style={{ color: 'black', textDecoration: 'none' }}>
+                              {theme.title}
+                            </Link>
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  );
                 })
               }
             </Grid>
@@ -158,60 +154,6 @@ const Themes: React.FC = () => {
           <></>
         )
       }
-      {/* <Dialog
-        open={userDetailOpen}
-        keepMounted
-        onClose={() => setUserDetailOpen(false)}
-      >
-        <DialogContent>
-          <Grid container justify="center">
-            <Grid item>
-              <Avatar
-                alt="avatar"
-                src={user?.image.url}
-                className={classes.avatar}
-              />
-            </Grid>
-          </Grid>
-          <Grid container justify="center">
-            <Grid item style={{ marginTop: "1rem" }}> */}
-              {/* <Typography variant="body1" component="p" gutterBottom style={{ textAlign: "center" }}>
-                {user.name} {userAge()}歳 ({userPrefecture()})
-              </Typography> */}
-              {/* <Divider />
-              <Typography
-                variant="body2"
-                component="p"
-                gutterBottom
-                style={{ marginTop: "0.5rem", fontWeight: "bold" }}
-              >
-                自己紹介
-              </Typography>
-              <Typography variant="body2" component="p" color="textSecondary" style={{ marginTop: "0.5rem" }}>
-                {user.profile ? user.profile : "よろしくお願いします。" }
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container justify="center">
-            <Button
-              variant="outlined"
-              onClick={() => isLikedUser(user.id) ? void(0) : handleCreateLike(user)}
-              color="secondary"
-              startIcon={isLikedUser(user.id) ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-              disabled={isLikedUser(user.id) ? true : false}
-              style={{ marginTop: "1rem", marginBottom: "1rem" }}
-            >
-              {isLikedUser(user.id) ? "いいね済み" : "いいね"}
-            </Button>
-          </Grid>
-        </DialogContent>
-      </Dialog> */}
-      {/* <AlertMessage
-        open={alertMessageOpen}
-        setOpen={setAlertMessageOpen}
-        severity="success"
-        message="マッチングが成立しました!"
-      /> */}
     </>
   )
 }
--- src/interfaces/theme.ts
+++ src/interfaces/theme.ts
@@ -9,6 +9,19 @@ export interface Theme {
   links?: Link[]
 }
 
+export interface GetThemesResponse {
+  themeId: number
+  title: string
+  postStatus: number
+  createdAt: Date
+  updatedAt: Date
+  user: {
+    userId: number
+    name: string
+    image: string
+  }
+}
+
 export interface PostThemeRequest {
   title: string
 }
